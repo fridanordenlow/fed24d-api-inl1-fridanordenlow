1a2a113e9d40710ce0e0e8b45b316c3795ac39e4
Frida Nordenlöw <frida.nordenlow@hotmail.com>
2025-04-17 12:24:41 +0200

    feat: set up api route to add product

--- src/controller/productController.ts
+++ src/controller/productController.ts
@@ -6,7 +6,7 @@ import { parsePrice } from '../helpers/parsePrice';
 
 export const fetchAllProducts = async (req: Request, res: Response) => {
   try {
-    const sql = 'SELECT * FROM products';
+    const sql = `SELECT * FROM products`;
     const [rows] = await db.query<IProduct[]>(sql);
 
     // Convert DECIMAL prices to numbers
@@ -27,7 +27,10 @@ export const fetchSingleProduct = async (req: Request, res: Response) => {
   const id = req.params.id;
 
   try {
-    const sql = 'SELECT * FROM products WHERE id = ?';
+    const sql = `
+    SELECT * FROM products 
+    WHERE id = ?
+    `;
     const [rows] = await db.query<IProduct[]>(sql, [id]);
     const product = rows[0];
 
@@ -39,6 +42,34 @@ export const fetchSingleProduct = async (req: Request, res: Response) => {
 
     console.log(parsedProduct);
     res.json(parsedProduct);
+  } catch (error: unknown) {
+    const message = error instanceof Error ? error.message : 'Unknown error';
+    res.status(500).json({ error: message });
+  }
+};
+
+export const addProduct = async (req: Request, res: Response) => {
+  const { name, description, stock, price, image_url } = req.body;
+
+  if (!name || !description || !stock || !price || !image_url) {
+    res.status(400).json({
+      error: 'Name, description, stock, price and image_url are required',
+    });
+  }
+
+  try {
+    const sql = `
+    INSERT INTO products (name, description, stock, price, image_url)
+    VALUES (?, ?, ?, ?, ?)
+    `;
+    const [result] = await db.query<ResultSetHeader>(sql, [
+      name,
+      description,
+      stock,
+      price,
+      image_url,
+    ]);
+    res.status(201).json({ message: 'Product added', id: result.insertId });
   } catch (error: unknown) {
     const message = error instanceof Error ? error.message : 'Unknown error';
     res.status(500).json({ error: message });
--- src/routers/productsRouter.ts
+++ src/routers/productsRouter.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import {
+  addProduct,
   fetchAllProducts,
   fetchSingleProduct,
 } from '../controller/productController';
@@ -9,7 +10,7 @@ const router = express.Router();
 // The route has path "products" written in index.ts
 router.get('/', fetchAllProducts);
 router.get('/:id', fetchSingleProduct);
-// router.post('/', fetch); // Skapa en produkt
+router.post('/', addProduct); // Skapa en produkt
 // router.patch('/:id', fetch); // Uppdatera en produkt
 // router.delete('/:id', fetch); // Radera en produkt
 
