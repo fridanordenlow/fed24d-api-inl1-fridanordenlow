1d1dc1544ed69eb689c1e7aa194d7a13e02c3f8e
Frida Nordenlöw <frida.nordenlow@hotmail.com>
2025-04-22 14:01:12 +0200

    delete parsePrice helper function

--- src/helpers/parsePrice.ts
+++
@@ -1,8 +0,0 @@
-import { IProduct } from '../models/IProduct';
-
-export const parsePrice = (product: IProduct): IProduct => {
-  return {
-    ...product,
-    price: parseFloat(product.price as unknown as string),
-  };
-};
